af1fe01b0d9110c3c81530afa66bcc6ec63ad9a2
Guddishikha <gshikha@anviam.com>
2021-11-10 17:44:29 +0530

    sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
+  end
   root to: "user#index"
   resources :albums do
     member do
